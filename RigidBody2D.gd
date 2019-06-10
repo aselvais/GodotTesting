b87954596d70af44e465b7901b8fbb3423469f05
aselvais <Antidot3@>
2019-06-10 17:41:39 -0400

    some changes to the GUI

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -3,10 +3,6 @@ extends RigidBody2D
 var thrust = Vector2(0, 250)
 var torque = 20000
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
